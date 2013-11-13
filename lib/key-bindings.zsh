27252951cdf5233fcaaaad958fcad724a9c346f9
ShadowStar <orphen.leiliu@gmail.com>
2013-11-13 12:49:38 +0800

    Changes the key bindings of '^[[A' & '^[[B'

--- lib/key-bindings.zsh
+++ lib/key-bindings.zsh
@@ -8,8 +8,10 @@ bindkey "^[[5~" up-line-or-history
 bindkey "^[[6~" down-line-or-history
 
 # make search up and down work, so partially type and hit up/down to find relevant stuff
-bindkey '^[[A' up-line-or-search
-bindkey '^[[B' down-line-or-search
+bindkey '^[[A' history-beginning-search-backward
+bindkey '^[[B' history-beginning-search-forward
+#bindkey '^[[A' up-line-or-search
+#bindkey '^[[B' down-line-or-search
 
 bindkey "^[[H" beginning-of-line
 bindkey "^[[1~" beginning-of-line
