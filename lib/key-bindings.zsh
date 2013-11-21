5e5d55b7780b41ef3978fde39dad2e230bdc7d9c
ShadowStar <orphen.leiliu@gmail.com>
2013-11-21 14:13:44 +0800

    Restore key-bind of '^[[A' & '^[[B', and binding to '^K' & '^J'

--- lib/key-bindings.zsh
+++ lib/key-bindings.zsh
@@ -8,10 +8,10 @@ bindkey "^[[5~" up-line-or-history
 bindkey "^[[6~" down-line-or-history
 
 # make search up and down work, so partially type and hit up/down to find relevant stuff
-bindkey '^[[A' history-beginning-search-backward
-bindkey '^[[B' history-beginning-search-forward
-#bindkey '^[[A' up-line-or-search
-#bindkey '^[[B' down-line-or-search
+bindkey '^K' history-beginning-search-backward
+bindkey '^J' history-beginning-search-forward
+bindkey '^[[A' up-line-or-search
+bindkey '^[[B' down-line-or-search
 
 bindkey "^[[H" beginning-of-line
 bindkey "^[[1~" beginning-of-line
