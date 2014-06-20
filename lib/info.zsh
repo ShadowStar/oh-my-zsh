10cadbe3f7e8cea76bd8e38cdf3913e0e441815f
ShadowStar <orphen.leiliu@gmail.com>
2014-06-20 18:13:56 +0800

    Update info.zsh
    Show user-info on remote and add prefix `@' to remote IP

--- lib/info.zsh
+++ lib/info.zsh
@@ -1,11 +1,11 @@
 function remote_info() {
 	if [ -n "$SSH_CONNECTION" ]; then
-		echo "`echo $SSH_CONNECTION | awk '{ print $3 }'` "
+		echo "@`echo $SSH_CONNECTION | awk '{ print $3 }'` "
 	fi
 }
 
 function user_info() {
-	if [[ "$DISABLE_USER_INFO" != "true" ]]; then
+	if [[ "$DISABLE_USER_INFO" != "true" || -n "$SSH_CONNECTION" ]]; then
 		if [ -n "$SUDO_USER" ]; then
 			echo "${SUDO_USER}[S] "
 		else
