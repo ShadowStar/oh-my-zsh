64c2ac3d38d0ba631a401f68137a7bf3210a884c
ShadowStar <orphen.leiliu@gmail.com>
2017-12-06 01:19:23 +0800

    Fix (lib git): Fix to get correct branch name while just ``checkout -b''

--- lib/git.zsh
+++ lib/git.zsh
@@ -2,9 +2,9 @@
 function git_prompt_info() {
   local ref
   if [[ "$(command git config --get oh-my-zsh.hide-status 2>/dev/null)" != "1" ]]; then
-    ref=$(command git describe --all --tags HEAD 2>/dev/null) || \
+    ref=$(command git symbolic-ref -q HEAD 2>/dev/null) || \
     ref=$(command git rev-parse --short HEAD 2>/dev/null) || return 0
-    echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
+    echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref##refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
   fi
 }
 
