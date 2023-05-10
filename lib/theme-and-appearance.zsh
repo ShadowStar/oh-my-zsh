d660edd18d9e4be26f62dd038bcb8bee243c7da8
ShadowStar <orphen.leiliu@gmail.com>
2023-05-11 00:42:44 +0800

    Feature (lib/theme-and-appearance.zsh): Change output color of BSD-based 					`ls' as the same as in Linux

--- lib/theme-and-appearance.zsh
+++ lib/theme-and-appearance.zsh
@@ -24,7 +24,7 @@ fi
 [[ "$DISABLE_LS_COLORS" != true ]] || return 0
 
 # Default coloring for BSD-based ls
-export LSCOLORS="Gxfxcxdxbxegedabagacad"
+export LSCOLORS="exfxcxdxbxegedabagacad"
 export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43'
 
 # Default coloring for GNU-based ls
